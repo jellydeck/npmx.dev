2b855e34b0411361d280c715ed3968bce34cbf77
jellydeck <91427591+jellydeck@users.noreply.github.com>
2026-02-12 18:57:21 +0530

    use scale-100 instead of scale-1

--- app/components/Button/Group.vue
+++ app/components/Button/Group.vue
@@ -7,7 +7,7 @@ const props = defineProps<{
 <template>
   <component
     :is="props.as || 'div'"
-    class="inline-flex items-stretch [&>*:not(:first-child)]:(-ms-px rounded-is-none) [&>*:not(:last-child)]:rounded-ie-none [&>*:hover]:(relative z-40 scale-1) [&>*:active]:scale-1 [&>*:focus-visible]:(relative z-40)"
+    class="inline-flex items-stretch [&>*:not(:first-child)]:(-ml-px rounded-is-none) [&>*:not(:last-child)]:rounded-ie-none [&>*]:(relative) [&>*:hover]:(z-10 !scale-100) [&>*:focus-visible]:(z-40) [&>*:active]:(z-10 !scale-100)"
   >
     <slot />
   </component>
